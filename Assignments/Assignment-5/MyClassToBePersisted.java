40e6b31839c673459e7ac690385c3ca639abf76c
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-05 02:14:41 +0530

    Use new constructors

--- Assignments/Assignment-5/MyClassToBePersisted.java
+++ Assignments/Assignment-5/MyClassToBePersisted.java
@@ -4,14 +4,8 @@ import java.io.ObjectOutputStream;
 
 public class MyClassToBePersisted {
     public static void main(String[] args) {
-        Profile pr1 = new Profile();
-        School sc1 = new School();
-        pr1.setName("Abhishek");
-        pr1.setAge(10);
-        pr1.setHobby("Video Games");
-        sc1.setNameOfSchool("Kid Zone");
-        sc1.setYearStarted(1998);
-        pr1.setSchool(sc1);
+        School sc1 = new School("Kid Zone", 1998);
+        Profile pr1 = new Profile("Abhishek", 10, "Video Games", sc1);
 
         try {
             FileOutputStream fos = new FileOutputStream("./Profile.ser");
