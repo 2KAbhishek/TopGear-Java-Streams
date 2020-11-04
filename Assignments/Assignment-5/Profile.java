7e7bec0e414a7f45b3b92a3d6efd96265828de80
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-04 23:14:24 +0530

    Add Profile.java

---
+++ Assignments/Assignment-5/Profile.java
@@ -0,0 +1,38 @@
+public class Profile {
+    private String name;
+    private int age;
+    private String hobby;
+    private School school;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public School getSchool() {
+        return school;
+    }
+
+    public void setSchool(School school) {
+        this.school = school;
+    }
+}
