dcee80e855f486b51d9f6b6da59ae33a9131096d
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-11-05 01:35:34 +0530

    Add School.java

---
+++ Assignments/Assignment-5/School.java
@@ -0,0 +1,22 @@
+import java.io.Serializable;
+
+public class School implements Serializable {
+    private String nameOfSchool;
+    private transient int yearStarted;
+
+    public String getNameOfSchool() {
+        return nameOfSchool;
+    }
+
+    public void setNameOfSchool(String nameOfSchool) {
+        this.nameOfSchool = nameOfSchool;
+    }
+
+    public int getYearStarted() {
+        return yearStarted;
+    }
+
+    public void setYearStarted(int yearStarted) {
+        this.yearStarted = yearStarted;
+    }
+}
